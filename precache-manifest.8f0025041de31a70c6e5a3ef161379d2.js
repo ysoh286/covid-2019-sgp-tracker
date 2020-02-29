df527354dba9bc1344969c834b52227451723670
ysoh286 <ysoh286@gmail.com>
2020-02-29 23:36:03 +1300

    Updates

---
+++ precache-manifest.8f0025041de31a70c6e5a3ef161379d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4bfd588f199e417e06cbe4d6bffa6f",
+    "url": "/ncovid-2019-sgp-tracker/index.html"
+  },
+  {
+    "revision": "fc530c45b1f1ec9d83cf",
+    "url": "/ncovid-2019-sgp-tracker/static/css/main.e832d33b.chunk.css"
+  },
+  {
+    "revision": "a0f20c265a6444fe0202",
+    "url": "/ncovid-2019-sgp-tracker/static/js/2.bb695523.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/ncovid-2019-sgp-tracker/static/js/2.bb695523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc530c45b1f1ec9d83cf",
+    "url": "/ncovid-2019-sgp-tracker/static/js/main.2f6bbcdf.chunk.js"
+  },
+  {
+    "revision": "1c69dc134d23c58dce77",
+    "url": "/ncovid-2019-sgp-tracker/static/js/runtime-main.cd6585ad.js"
+  },
+  {
+    "revision": "b71c98e41bb412e9e2c431e03cb85f4d",
+    "url": "/ncovid-2019-sgp-tracker/static/media/coronavirus-artwork.b71c98e4.jpg"
+  },
+  {
+    "revision": "2e5e8603756796d0047b4f76f5fce679",
+    "url": "/ncovid-2019-sgp-tracker/static/media/ncovid-2019-sgp.2e5e8603.csv"
+  }
+]);
